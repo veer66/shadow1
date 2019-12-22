22fcd6ccb0ceb97b403c8df13a2cb6a1c4d0348e
Vee Satayamas <vee.sa@protonmail.com>
2019-12-22 21:24:11 +0700

    re-frame

--- src/main/webapp/core.cljs
+++ src/main/webapp/core.cljs
@@ -1,13 +1,33 @@
 (ns webapp.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]))
 
 (def msg "Hello")
+
+(rf/reg-event-db
+ :initialize
+ (fn [_ _]
+   {:cnt 1}))
+
+(rf/reg-event-db
+ :plusone
+ (fn [db _]
+   (update db :cnt inc)))
+
+(rf/reg-sub
+ :cnt
+ (fn [db _]
+   (:cnt db)))
 
 (defn ui
   []
   [:div
    [:h1 "DEMO"]
-   [:p msg]])
+   [:p msg]
+   [:p @(rf/subscribe [:cnt])]
+   [:input {:type "button"
+            :value "+1"
+            :on-click #(rf/dispatch [:plusone])}]])
 
 (defn render
   []
@@ -15,9 +35,9 @@
             (js/document.querySelector "div#app")))
 
 (defn ^:dev/after-load reload []
+  (rf/clear-subscription-cache!)
   (render))
 
 (defn init []
+  (rf/dispatch-sync [:initialize])
   (render))
-
-
