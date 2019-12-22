413207ee87e6644e27abaa77930edd46d471529b
Vee Satayamas <vee.sa@protonmail.com>
2019-12-22 20:30:18 +0700

    Demo hot reload

--- src/main/webapp/core.cljs
+++ src/main/webapp/core.cljs
@@ -1,6 +1,24 @@
 (ns webapp.core)
 
-(defn init []
+(def msg "Hello")
+
+(defn remove-all-children [elem]
+  (loop [elem elem]
+    (when-let [child (.-firstChild elem)]
+      (do
+        (.removeChild elem child)
+        (recur elem)))))
+
+(defn render []
   (let [p-main (js/document.querySelector "p#main")
-        txt (js/document.createTextNode "FROM shadow1.webapp.core/init")]
+        txt (js/document.createTextNode msg)]
+    (js/console.log (remove-all-children p-main))
     (.appendChild p-main txt)))
+
+(defn ^:dev/after-load reload []
+  (render))
+
+(defn init []
+  (render))
+
+
